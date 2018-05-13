de5c910754d56bdba8e65c9bcf91cacbe78e3211
mkdgo <dimitar.ovcharov70@gmail.com>
2018-05-13 19:50:48 +0300

    general language management

--- init.php
+++ init.php
@@ -1,14 +1,33 @@
 <?php
-    $ln = 'en';
-    if( isset($_GET['ln']) ) {
-        $ln = trim($_GET['ln']);
-        if( !isset($lang[$ln]) ) {
-            $ln = 'en';
-        }
+
+define('ROOT_PATH', realpath(dirname(__FILE__)));
+define('PUBLIC_PATH', '/');
+//echo ROOT_PATH;die;
+$ln = 'en';
+$file_name = 'lang_en.php';
+$lang_file =  ROOT_PATH . '/' . $file_name;
+$cookie_name = "lang";
+$cookie_value = "en";
+if( isset($_GET['lang']) ) {
+    $ln = trim($_GET['lang']);
+    $file_name = 'lang_'.$ln.'.php';
+    if( is_file( ROOT_PATH . '/' . $file_name ) ) {
+        $lang_file =  ROOT_PATH . '/' . $file_name;
+        $cookie_value = $ln;
+        setcookie($cookie_name, $cookie_value, time() + (86400 * 30));
+    } elseif( isset($_COOKIE[$cookie_name]) ) {
+        $ln = $_COOKIE[$cookie_name];
+        $lang_file =  ROOT_PATH . '/' . $cookie_name . '_' . $ln . '.php';
+    } else {
+        $ln = 'en';
     }
-    include 'languages.php';
-    $current_lang = $lang[$ln];
-    include 'head.php';
+} elseif( isset($_COOKIE[$cookie_name]) ) {
+    $ln = $_COOKIE[$cookie_name];
+    $lang_file =  ROOT_PATH . '/' . $cookie_name . '_' . $ln . '.php';
+}
+include $lang_file;
+$current_lang = $lang[$ln];
+include 'head.php';
 ?>
     <div class="<?php echo $class_wrapper; ?> wrapper">    
     <?php include 'header.php'; ?>    
